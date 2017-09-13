d2a4926ad29be1abe198ac140e0de79caf1d2309
khthomas <khthomas@smu.edu>
2017-09-12 20:57:56 -0400

    Update power_profiles.py

--- Power_Profiles/power_profiles.py
+++ Power_Profiles/power_profiles.py
@@ -17,6 +17,7 @@ To Do:
     2. Find a way to include title, xlabel, and ylabel
     3. Update daily function to plot the day by day power consumption (for historical analysis)
     4. incorporate proper datetime?
+    5. Clean file to remove uneeded test code
 """
 
 import pandas as pd
